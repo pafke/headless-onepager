876c3bfb4a35f82d0847184e52639d4db947c573
timo <timo@blueminds.nl>
2018-10-22 18:06:11 +0200

    CONTACTFORM: Netlify build test

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,7 +3,30 @@ import React from 'react'
 const Contact = ({allotText}) => (
     <section>
         <h1>Gunwerk</h1>
-        <div class="content-left" dangerouslySetInnerHTML={{__html: allotText}}>
+        <div className="content-left" dangerouslySetInnerHTML={{__html: allotText}}>
+        </div>
+        <div className="content-right">
+            <form name="contactForm" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+                <div className="group">
+                    <input name="naam" type="text" required />
+                    <span className="highlight"></span>
+                    <span className="bar"></span>
+                    <label>Naam</label>
+                </div>
+                <div className="group">
+                    <input name="email" type="text" required />
+                    <span className="highlight"></span>
+                    <span className="bar"></span>
+                    <label>Email</label>
+                </div>
+                <div className="group">
+                    <textarea id="message" rows="5" name="bericht" required></textarea>
+                    <span className="highlight"></span>
+                    <span className="bar"></span>
+                    <label>Project beschrijving</label>
+                </div>
+                <input type="submit" value="Gunnen man!" />
+            </form>
         </div>
     </section>
 )
--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Footer = () => (
     <section>
-        Footer
+        <h2>Contact</h2>
+        <a href="tel:+31643511371">T. 06 435 11 371</a>
+        <a href="mailto:mail@timoveld.nl">M. mail@timoveld.nl</a>
+        <p>
+            Voor vragen, eventuele samenwerking, of gewoon omdat het kan, mag je altijd contact met mij opnemen.
+        </p>
     </section>
 )
 
