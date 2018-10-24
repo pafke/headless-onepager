4b7f017947d216b68ea50f84be595b3fc5687c34
Sophie <sophie@gsdh.org>
2018-10-24 20:01:10 +0200

    TEST

--- src/components/contact.js
+++ src/components/contact.js
@@ -7,27 +7,11 @@ const Contact = ({allotText}) => (
             <div className="content-left" dangerouslySetInnerHTML={{__html: allotText}}>
             </div>
             <div className="content-right">
-                <form name="contactForm" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-                    <div className="group">
-                        <input name="naam" type="text"/>
-                        <span className="highlight"></span>
-                        <span className="bar"></span>
-                        <label>Naam</label>
-                    </div>
-                    <div className="group">
-                        <input name="email" type="text"/>
-                        <span className="highlight"></span>
-                        <span className="bar"></span>
-                        <label>Email</label>
-                    </div>
-                    <div className="group">
-                        <textarea id="message" rows="5" name="bericht"></textarea>
-                        <span className="highlight"></span>
-                        <span className="bar"></span>
-                        <label>Project beschrijving</label>
-                    </div>
-                    <button>Gunnen man!</button>
-                </form>
+             	<form name="contact" netlify netlify-honeypot="bot-field" hidden>
+    	      		<input type="text" name="name" />
+        	      	<input type="email" name="email" />
+        	      	<textarea name="message"></textarea>
+        	    </form>
             </div>
         </div>
     </section>
