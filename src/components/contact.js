ace9b75620a4d064b781849b698acecbf3a46db7
Sophie <sophie@gsdh.org>
2018-10-24 20:09:00 +0200

    TEST

--- src/components/contact.js
+++ src/components/contact.js
@@ -12,6 +12,21 @@ const Contact = ({allotText}) => (
         	      	<input type="email" name="email" />
         	      	<textarea name="message"></textarea>
         	    </form>
+        	    <form name="contact" method="post">
+        	              <input type="hidden" name="form-name" value="contact" />
+        	              <p>
+        	                <label>Your Name: <input type="text" name="name"/></label>
+        	              </p>
+        	              <p>
+        	                <label>Your Email: <input type="email" name="email"/></label>
+        	              </p>
+        	              <p>
+        	                <label>Message: <textarea name="message"></textarea></label>
+        	              </p>
+        	              <p>
+        	                <button type="submit">Send</button>
+        	              </p>
+        	            </form>
             </div>
         </div>
     </section>
