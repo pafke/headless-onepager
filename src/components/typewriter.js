beb100a0cd41747a58cecf53dba156caca3421db
Timo <mail@timoveld.nl>
2018-11-19 22:37:15 +0100

    TYPEWRITER: Added component for intro text effect

--- src/components/typewriter.js
+++ src/components/typewriter.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import HotTyper from 'react-hot-typer';
 
 const Typewriter = () => (
     <section className="bg-green fullscreen">
-        Typewriter
+    	I
+        <HotTyper
+	        text={[
+	        	'Create',
+	        	'Develop',
+	        	'Render',
+	        	'Design',
+	        	'Debug',
+	        	'Visualise',
+	        	'Animate'
+	        ]}
+	        speedOfLoop={1000}
+	        highlightDuration={60000}
+	    /><br />
+	    therefore I am.
     </section>
 )
 
