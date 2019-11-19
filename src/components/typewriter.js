288ada1ab10fdccf33068749bc5fbbdc8ad0326a
timo <timo@blueminds.nl>
2019-11-19 16:06:20 +0100

    SPLASHLOGO: Als SVG er in gezet

--- src/components/typewriter.js
+++ src/components/typewriter.js
@@ -33,6 +33,10 @@ const TypeWriterSection = styled(BgGreen)`
         font-size: 78px;
     }
 `;
+const SplashLogo = styled.svg`
+    border: solid #FFF 1px;
+    border-radius: 50%;
+`;
 
 class TypeEffect extends React.Component {
     constructor(props) {
@@ -172,6 +176,15 @@ const Typewriter = () => (
         <div>
             therefore I am.
         </div>
+        <SplashLogo xmlns="http://www.w3.org/2000/svg" version="1.1" x="0px" y="0px" width="139px" height="139px" viewBox="-1 0 142 139" enable-background="new 0 0 139.001 139.001">
+            <g>
+                <circle fill="none" cx="69.501" cy="69.501" r="69.501"></circle>
+                <g>
+                    <polygon fill="none" stroke="#FFFFFF" stroke-miterlimit="10" points="69.521,99.461 20.497,50.437 118.544,50.437   "></polygon>
+                    <line fill="none" stroke="#FFFFFF" stroke-miterlimit="10" x1="69.521" y1="99.461" x2="69.521" y2="50.437"></line>
+                </g>
+            </g>
+        </SplashLogo>
     </TypeWriterSection>
 );
 
