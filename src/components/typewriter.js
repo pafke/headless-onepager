91286aec5854665b3134653587fe2d601bff7aca
timo <timo@blueminds.nl>
2019-11-19 09:55:33 +0100

    TYPEWRITER: ieder deel eigen state gegeven zodat styling van selectie weg is na typen

--- src/components/typewriter.js
+++ src/components/typewriter.js
@@ -3,15 +3,20 @@ import React from 'react';
 class TypeEffect extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { randomVerb: false};
+        this.state = {
+            randomVerb: false,
+            randomVerbKeepPart: false,
+            randomVerbReplacePart: false,
+            randomVerbTypeNewPart: false
+        };
     }
     componentDidMount = () => {
-        this.setState({randomVerb: [this._getRandomVerb()]});
+        this.setState({randomVerb: this._getRandomVerb()});
         this._startCounterNewVerb();
     }
     _getRandomVerb = () => {
         let randomVerb = this.props.verbs[Math.floor(Math.random()*this.props.verbs.length)];
-        if(randomVerb === this.state.randomVerb[0]) {
+        if(randomVerb === this.state.randomVerb) {
             console.log('Retrying');
             return this._getRandomVerb();
         } else {
@@ -20,17 +25,17 @@ class TypeEffect extends React.Component {
     }
     _startCounterNewVerb = () => {
         clearTimeout(this.counterNewVerb);
-        const counterNewVerb = this._counterNewVerb;
+        const counterNewVerb = this._counterHighlightVerb;
         this.counterNewVerb = setTimeout(function() {
             counterNewVerb();
         }, this.props.speedOfLoop);
     }
-    _counterNewVerb = () => {
+    _counterHighlightVerb = () => {
         clearTimeout(this.counterHighlight)
         //Verschil tussen nieuwe waardes tonen
         let newVerb = this._getRandomVerb();
         let newVerbSeperated = newVerb.split('');
-        let currentVerbSeperated = this.state.randomVerb[0].split('');
+        let currentVerbSeperated = this.state.randomVerb.split('');
         let sameCharacters = 0;
         for (var i = 0; i < newVerbSeperated.length; i++) {
             if(currentVerbSeperated[i] && newVerbSeperated[i] === currentVerbSeperated[i]) {
@@ -44,8 +49,12 @@ class TypeEffect extends React.Component {
             keepCharacters = keepCharacters.join('');
             replaceCharacters = replaceCharacters.join('');
         let newCharacters = newVerbSeperated.slice(sameCharacters, newVerbSeperated.length);
-        this.setState({randomVerb: [keepCharacters, replaceCharacters]});
-        //let restartVerb = this._restartVerb;
+        this.setState({
+            randomVerb: false,
+            randomVerbKeepPart: keepCharacters,
+            randomVerbReplacePart: replaceCharacters,
+            //randomVerbTypeNewPart: false
+        });
         let typeNewVerb = this._typeNewVerb;
         this.counterHighlight = setTimeout(function() {
             typeNewVerb(newVerb, newCharacters);
@@ -53,7 +62,7 @@ class TypeEffect extends React.Component {
     }
     _setTimeoutForTyping = (typeOutNewVerb, index, newVerb) => {
         const setStateNewWord = this._setStateTypeNewVerb;
-        const origCharacters = this.state.randomVerb[0];
+        const origCharacters = this.state.randomVerbKeepPart;
         const restartVerb = this._restartVerb;
         setTimeout(
             function() {
@@ -67,7 +76,10 @@ class TypeEffect extends React.Component {
         );
     }
     _setStateTypeNewVerb = (typeOutNewVerb, index) => {
-        this.setState({randomVerb: [this.state.randomVerb[0], typeOutNewVerb]});
+        this.setState({
+            randomVerbReplacePart: false,
+            randomVerbTypeNewPart: typeOutNewVerb
+        });
     }
     _typeNewVerb = (newVerb, newCharacters) => {
         let typeOutNewVerb = '';
@@ -78,21 +90,25 @@ class TypeEffect extends React.Component {
         }
     }
     _restartVerb = (newVerb) => {
-        //Hier zorgen dat nieuwe letters uitgetypt worden
-        this.setState({randomVerb: [newVerb]});
+        this.setState({
+            randomVerb: newVerb,
+            randomVerbKeepPart: false,
+            randomVerbReplacePart: false,
+            randomVerbTypeNewPart: false
+        });
         this._startCounterNewVerb();
     }
     render() {
-
-        let verbReplacePart;
-        if (this.state.randomVerb[1]) {
-            verbReplacePart = <span className="highlighted">{this.state.randomVerb[1]}</span>;
+        let randomVerbReplacePart;
+        if (this.state.randomVerbReplacePart) {
+            randomVerbReplacePart = <span className="highlighted">{this.state.randomVerbReplacePart}</span>;
         }
-
         return (
             <div>
-                {this.state.randomVerb[0]}
-                {verbReplacePart}
+                {this.state.randomVerb}
+                {this.state.randomVerbKeepPart}
+                {randomVerbReplacePart}
+                {this.state.randomVerbTypeNewPart}
             </div>
         );
     }
