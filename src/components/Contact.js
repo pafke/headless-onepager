45cffa0427b11c1e2f6a96ca07a6078554182a1d
timo <timo@blueminds.nl>
2020-12-04 21:55:15 +0100

    FORM: react variant van form geimplementeerd

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -26,7 +26,7 @@ function Contact(props) {
                 <LeftContent dangerouslySetInnerHTML={{__html: textPartial}}>
                 </LeftContent>
                 <RightContent>
-                    <form name="contact" method="POST" data-netlify="true">
+                    <form name="contact" method="post">
                         <input type="hidden" name="form-name" value="contact" />
                         <p>
                             <label>Your Name: <input type="text" name="name"/></label>
